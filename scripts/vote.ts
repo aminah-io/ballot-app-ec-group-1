285f8e604bf1db422630f997a3e5af0fa6941cf0
kilohash <r-type@ukr.net>
2023-08-06 21:17:40 +0300

    Drafted cast a vote script

---
+++ scripts/vote.ts
@@ -0,0 +1,29 @@
+import { ethers } from "ethers"
+import { Ballot, Ballot__factory } from "../typechain-types";
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+async function main() {
+    const parameter = process.argv.slice(2);
+    // const contractAddress = parameter[0];
+
+    const provider = new ethers.JsonRpcProvider(process.env. RPC_ENDPOINT_URL ?? "") ;
+
+    // Pick the option that has been configured in .env
+    const wallet = new ethers.Wallet(process.env.MNEMONIC ?? "", provider);
+    // const wallet = new ethers.Wallet (process.env.PRIVATE_KEY ?? "", provider);
+
+    console.log(`Using address ${wallet.address}`);
+    const balanceBN = await provider. getBalance(wallet.address);
+    const balance = Number (ethers. formatUnits (balanceBN));
+    console. log('Wallet balance ${balance)');
+
+    if (balance < 0.01) {
+        throw new Error("Not enough ethers")
+    }
+
+    const ballotfactory = new Ballot__factory(wallet);
+    const ballotContract = ballotfactory.attach("0xF6b84057fa1188E99c257d80d96e5C8EE0192992") as Ballot;
+
+    const tx = await ballotContract.vote(Number(parameter))
+}
