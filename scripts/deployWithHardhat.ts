fa8c16c09d123c71f97f74c532f7b15d1eca07e5
Aminah Burch <hello@aminah.io>
2023-08-06 16:05:49 +0300

    Updates hardhat deploy script

--- scripts/deployWithHardhat.ts
+++ scripts/deployWithHardhat.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+const PROPOSALS = ["Coffee", "Tea", "Juice", "Water"]
 
 async function main() {
   const proposals = process.argv.slice(2);
@@ -20,18 +20,6 @@ async function main() {
   const address = await ballotContract.getAddress();
 
   const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
-  const lastBlock = await provider.getBlock("latest");
-  console.log({lastBlock});
-
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider)
-
-  console.log(`Using address ${wallet.address}`);
-  const balanceBN = await provider.getBalance(wallet.address);
-  const balance = Number(ethers.formatUnits(balanceBN));
-  console.log(`Wallet balance ${balance}`);
-  if (balance < 0.01) {
-    throw new Error("Not enough ether");
-  }
   
   console.log(`Contract deployed at address: ${address}`);
   for (let i = 0; i < proposals.length; i++) {
@@ -39,7 +27,6 @@ async function main() {
     const name = ethers.decodeBytes32String(proposal.name);
     console.log({ i, name, proposal });
   }
-    
 }
 
 main().catch((error) => {
