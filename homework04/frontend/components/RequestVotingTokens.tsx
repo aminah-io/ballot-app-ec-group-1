3dd37d4f0bedb278917405a9938bf60b66d0a344
Aminah Burch <hello@aminah.io>
2023-08-20 22:42:44 +0300

    fixes up frontend

--- homework04/frontend/components/RequestVotingTokens.tsx
+++ homework04/frontend/components/RequestVotingTokens.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function RequestVotingTokens(params: {address: `0x${string}` | undefined}) {
   const [data, setData] = useState<any>(null);
   const [isLoading, setLoading] = useState(false);
+  const [voteAmount, setVoteAmount] = useState("");
   const options = {
     method: "POST",
     headers: {
@@ -12,7 +13,7 @@ export default function RequestVotingTokens(params: {address: `0x${string}` | un
   }
   
   const handleRequestVotingTokens = () => {
-    fetch(`http://localhost:3001/mint-tokens`, options)
+    fetch(`http://localhost:3001/request-voting-tokens`, options)
         .then((res) => res.json())
         .then((data) => {
           setData(data);
@@ -21,13 +22,32 @@ export default function RequestVotingTokens(params: {address: `0x${string}` | un
   }
 
   if (isLoading) return <div>Requesting tokens from API...</div>;
-  if (!data) return (
-    <button
-      className="rounded-full bg-indigo-600 px-2.5 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      onClick={handleRequestVotingTokens}>
-        Mint Tokens
-    </button>
-  );
+  if (!data) {
+    return (
+      <div className="flex flex-col justify-center">
+        <form className="flex flex-col justify-center mt-3">
+          <label>
+            Enter amount of voting token you want to request:
+          </label>
+          <input
+            className="mt-2"
+            type="number"
+            min={0.1}
+            max={1.0}
+            step={0.1}
+            placeholder="1"
+            value={voteAmount}
+            onChange={(e) => setVoteAmount(e.target.value)}
+          />
+        </form>
+        <button
+          className="mt-2 rounded-full bg-indigo-600 px-4 py-2 text-sm font-normal text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={handleRequestVotingTokens}>
+            Request Voting Tokens
+        </button>
+      </div>
+    );
+  }
   
   return (
     <div>
