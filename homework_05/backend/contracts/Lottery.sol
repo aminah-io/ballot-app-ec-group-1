27828d9482977bb790d0c71507ac21859a4e0c31
Aminah Burch <hello@aminah.io>
2023-08-29 19:08:33 +0300

    adds lottery files and updates

--- homework_05/backend/contracts/Lottery.sol
+++ homework_05/backend/contracts/Lottery.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { LotteryToken } from "./LotteryToken.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {LotteryToken} from "./LotteryToken.sol";
 
 /// @title A very simple lottery contract
 /// @author Matheus Pagani
